3b79b1adbf364092c989dd9449f5eabdbc7f9e6f
raisaafadilla <raisa.fadilla@ui.ac.id>
2023-09-20 02:38:58 +0700

    assignment 3

--- assignment2_pbp/urls.py
+++ assignment2_pbp/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
-from main.views import show_main
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
+    path('admin/', admin.site.urls),
 ]
--- main/models.py
+++ main/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    amount = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+    price = models.IntegerField()
     description = models.TextField()
