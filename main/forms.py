57a262e8838916902992d548cc981c19ab4dbf1a
raisaafadilla <raisa.fadilla@ui.ac.id>
2023-09-27 02:22:31 +0700

    assignment 4

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from main.models import Product
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "amounts", "description", "prices"]
+        fields = ["name", "amounts", "description", "prices", "user"]
